33918218bf9df9db0b64e259fb57f1b0ad69f6f3
Keith Goodman <kwgoodman@gmail.com>
2014-10-29 09:19:53 -0700

    WIP nansum

--- sandbox/rewrite.pyx
+++ sandbox/rewrite.pyx
@@ -9,13 +9,6 @@ from numpy cimport PyArray_EMPTY, PyArray_DIMS, import_array
 import_array()
 cdef double NAN = <double> NAN
 
-if np.int_ == np.int32:
-    NPY_int_ = NPY_INT32
-elif np.int_ == np.int64:
-    NPY_int_ = NPY_INT64
-else:
-    raise RuntimeError('Expecting default NumPy int to be 32 or 64 bit.')
-
 
 def nansum(arr, axis=None):
 
@@ -26,12 +19,7 @@ def nansum(arr, axis=None):
     else:
         a = np.array(arr, copy=False)
 
-    # dtype
     cdef int dtype = np.PyArray_TYPE(a)
-    if dtype < NPY_int_:
-        a = a.astype(np.int_)
-        dtype = np.PyArray_TYPE(a)
-
     cdef int ndim = np.PyArray_NDIM(a)
 
     # defend against 0d beings
@@ -57,6 +45,12 @@ def nansum(arr, axis=None):
     # what if axis is a float or such?
     cdef int axis_int = <int>axis
 
+    # defend against the evils of negative axes
+    if axis_int < 0:
+        axis_int += ndim
+        if axis_int < 0:
+            raise ValueError("axis(=%d) out of bounds" % axis)
+
     # reduce over all axes: 1d array, axis=0
     if ndim == 1 and axis_int == 0:
         if dtype == NPY_FLOAT64:
@@ -66,12 +60,6 @@ def nansum(arr, axis=None):
         if dtype == NPY_INT64:
             return nansum_int64_axisNone(a)
         raise TypeError("Unsupported dtype (%s)." % a.dtype)
-
-    # defend against the evils of negative axes
-    if axis < 0:
-        axis += ndim
-    if (axis >= ndim) or (axis < 0):
-        raise ValueError("axis(=%d) out of bounds" % axis)
 
     # reduce over a single axis
     if dtype == NPY_FLOAT64:
@@ -145,9 +133,13 @@ cdef inline np.int32_t nansum_int32_axisNone(np.ndarray a):
 
 cdef inline np.ndarray nansum_float64_axisint(np.ndarray a, int axis):
 
-
-    # temp hack
-    cdef int ndim = 2
+    cdef int ndim = a.ndim
+    if ndim <= 1:
+        raise ValueError("`a.ndim` must be greater than 1")
+    if axis < 0:
+        axis += ndim
+    if (axis >= ndim) or (axis < 0):
+        raise ValueError("axis(=%d) out of bounds" % axis)
 
     cdef np.flatiter ita
     ita = np.PyArray_IterAllButAxis(a, &axis)
