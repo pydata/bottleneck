b0248049f34e11362cadbd4c72b77f8aa5b9d066
Keith Goodman <kwgoodman@gmail.com>
2016-04-09 12:53:07 -0700

    Class (Threads) to help run multi threaded

--- bottleneck/__init__.py
+++ bottleneck/__init__.py
@@ -28,7 +28,7 @@ except:
 
 from bottleneck.version import __version__
 from bottleneck.benchmark.bench import bench
-from bottleneck.threads import run_threaded, make_unary
+from bottleneck.threads import Threads, make_unary
 
 try:
     from numpy.testing import Tester
--- bottleneck/threads.py
+++ bottleneck/threads.py
@@ -1,14 +1,36 @@
 from multiprocessing.pool import ThreadPool
+from bottleneck.benchmark.autotimeit import autotimeit
 
 
-def run_threaded(nthreads, array_list, func, **kwargs):
-    unary_func = make_unary(func, **kwargs)
-    try:
-        threadpool = ThreadPool(nthreads)
-        output_list = threadpool.map(unary_func, array_list)
-    finally:
-        threadpool.close()
-    return output_list
+class Threads(object):
+
+    def __init__(self, nthreads, func, **kwargs):
+        self.pool = ThreadPool(nthreads)
+        self.func = make_unary(func, **kwargs)
+        self.nthreads = nthreads
+        self.func_name = func.__name__
+        self.kwargs = kwargs
+
+    def run(self, arr_list):
+        return self.pool.map(self.func, arr_list)
+
+    def timeit(self, arr_list):
+        def non_threaded():
+            return map(self.func, arr_list)
+        t0 = autotimeit(non_threaded)
+        tmap = self.pool.map
+        def threaded():
+            return tmap(self.func, arr_list)
+        t1 = autotimeit(threaded)
+        return t0, t1
+
+    def __repr__(self):
+        txt = "%s(arr" % self.func_name
+        for key in self.kwargs:
+            txt += ", %s=%s" % (key, self.kwargs[key])
+        txt += ")"
+        txt += "; ThreadPool(%d)" % self.nthreads
+        return txt
 
 
 def make_unary(func, **kwargs):
