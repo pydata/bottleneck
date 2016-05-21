bb71ff85b044e4c207ac776822aa356df2cb844c
Midnighter <midnighter@posteo.net>
2016-05-22 00:23:24 +0200

    make activating the conda environment less verbose
    * change the environment name

--- ci/travis/conda_install.sh
+++ ci/travis/conda_install.sh
@@ -5,8 +5,11 @@ set -ev # exit on first error, print commands
 if [ "${PYTHON_ARCH}" == "32" ]; then
   set CONDA_FORCE_32BIT=1
 fi
-NAME="test-${PYTHON_VERSION}-${PYTHON_ARCH}bit"
+NAME="test-python-${PYTHON_VERSION}-${PYTHON_ARCH}bit"
 conda create -q -n "${NAME}" "${DEPS}" python="${PYTHON_VERSION}"
+
+set +v # we dont want to  see commands in the conda script
+
 source activate "${NAME}"
 conda info -a
 conda list
