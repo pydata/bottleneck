4c6398fd30bfe872db7ccf70973465221422266c
Midnighter <midnighter@posteo.net>
2016-05-22 00:59:53 +0200

    apply correct split without quotes

--- ci/travis/conda_install.sh
+++ ci/travis/conda_install.sh
@@ -7,7 +7,7 @@ if [ "${PYTHON_ARCH}" == "32" ]; then
 fi
 NAME="test-python-${PYTHON_VERSION}-${PYTHON_ARCH}bit"
 # split dependencies into separate packages
-IFS=" " DEPS=("${DEPS}")
+IFS=" " DEPS=(${DEPS})
 conda create -q -n "${NAME}" "${DEPS[@]}" python="${PYTHON_VERSION}"
 
 set +v # we dont want to  see commands in the conda script
