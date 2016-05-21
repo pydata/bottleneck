32f5119fc2f4d29154899d2e9e3685fd06ff441c
Midnighter <midnighter@posteo.net>
2016-05-22 00:39:24 +0200

    correctly handle multiple dependencies in script

--- ci/travis/conda_install.sh
+++ ci/travis/conda_install.sh
@@ -6,7 +6,7 @@ if [ "${PYTHON_ARCH}" == "32" ]; then
   set CONDA_FORCE_32BIT=1
 fi
 NAME="test-python-${PYTHON_VERSION}-${PYTHON_ARCH}bit"
-conda create -q -n "${NAME}" "${DEPS}" python="${PYTHON_VERSION}"
+conda create -q -n "${NAME}" "${DEPS[@]}" python="${PYTHON_VERSION}"
 
 set +v # we dont want to  see commands in the conda script
 
