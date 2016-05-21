bde6a8e7b15162f303a99ee43e7377cf7d63bf67
Midnighter <midnighter@posteo.net>
2016-05-22 00:50:43 +0200

    ensure that the dependencies string is split

--- ci/travis/conda_install.sh
+++ ci/travis/conda_install.sh
@@ -6,6 +6,8 @@ if [ "${PYTHON_ARCH}" == "32" ]; then
   set CONDA_FORCE_32BIT=1
 fi
 NAME="test-python-${PYTHON_VERSION}-${PYTHON_ARCH}bit"
+# split dependencies into separate packages
+IFS=" " DEPS=("${DEPS}")
 conda create -q -n "${NAME}" "${DEPS[@]}" python="${PYTHON_VERSION}"
 
 set +v # we dont want to  see commands in the conda script
