0f5227591fce85b2748e4d7a7810edbe406913ad
Keith Goodman <kwgoodman@gmail.com>
2016-06-12 09:40:01 -0700

    clean up setup.py

--- setup.py
+++ setup.py
@@ -19,10 +19,9 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
 
-modules = ['reduce', 'nonreduce', 'nonreduce_axis', 'move']
-
-
 def prepare_modules():
+
+    modules = ['reduce', 'nonreduce', 'nonreduce_axis', 'move']
 
     # Don't attempt to import numpy until it needed; this
     # enables pip to install numpy before bottleneck
@@ -31,6 +30,7 @@ def prepare_modules():
     # info needed to compile bottleneck
     kwargs = {}
     for module in modules:
+        # `-O3` causes slow down of nanmin and nanmax; force `-O2`
         kwargs[module] = {'include_dirs': [np.get_include()],
                           'extra_compile_args': ['-O2']}
         if module == 'move':
@@ -52,6 +52,24 @@ def prepare_modules():
                 for module in modules]
 
 
+def get_long_description():
+    with open('README.rst', 'r') as fid:
+        long_description = fid.read()
+    idx = max(0, long_description.find("Bottleneck is a collection"))
+    long_description = long_description[idx:]
+    return long_description
+
+
+def get_version_str():
+    ver_file = os.path.join('bottleneck', 'version.py')
+    with open(ver_file, 'r') as fid:
+        version = fid.read()
+    version = version.split("= ")
+    version = version[1].strip()
+    version = version.strip("\"")
+    return version
+
+
 CLASSIFIERS = ["Development Status :: 4 - Beta",
                "Environment :: Console",
                "Intended Audience :: Science/Research",
@@ -62,67 +80,28 @@ CLASSIFIERS = ["Development Status :: 4 - Beta",
                "Programming Language :: Python :: 3",
                "Topic :: Scientific/Engineering"]
 
-# Description
-description = "Fast NumPy array functions written in Cython"
-fid = open('README.rst', 'r')
-long_description = fid.read()
-fid.close()
-idx = max(0, long_description.find("Bottleneck is a collection"))
-long_description = long_description[idx:]
 
-# Get bottleneck version
-ver_file = os.path.join('bottleneck', 'version.py')
-fid = open(ver_file, 'r')
-VER = fid.read()
-fid.close()
-VER = VER.split("= ")
-VER = VER[1].strip()
-VER = VER.strip("\"")
-VER = VER.split('.')
-
-NAME = 'Bottleneck'
-MAINTAINER = "Keith Goodman"
-MAINTAINER_EMAIL = "bottle-neck@googlegroups.com"
-DESCRIPTION = description
-LONG_DESCRIPTION = long_description
-URL = "http://berkeleyanalytics.com/bottleneck"
-DOWNLOAD_URL = "http://pypi.python.org/pypi/Bottleneck"
-LICENSE = "Simplified BSD"
-CLASSIFIERS = CLASSIFIERS
-AUTHOR = "Keith Goodman"
-AUTHOR_EMAIL = "kwgoodman@gmail.com"
-PLATFORMS = "OS Independent"
-MAJOR = VER[0]
-MINOR = VER[1]
-MICRO = VER[2]
-ISRELEASED = False
-VERSION = '%s.%s.%s' % (MAJOR, MINOR, MICRO)
-PACKAGES = find_packages()
-PACKAGE_DATA = {'bottleneck': ['LICENSE']}
-REQUIRES = ["numpy"]
-
-metadata = dict(name=NAME,
-                maintainer=MAINTAINER,
-                maintainer_email=MAINTAINER_EMAIL,
-                description=DESCRIPTION,
-                long_description=LONG_DESCRIPTION,
-                url=URL,
-                download_url=DOWNLOAD_URL,
-                license=LICENSE,
+metadata = dict(name='Bottleneck',
+                maintainer="Keith Goodman",
+                maintainer_email="bottle-neck@googlegroups.com",
+                description="Fast NumPy array functions written in Cython",
+                long_description=get_long_description(),
+                url="https://github.com/kwgoodman/bottleneck",
+                download_url="http://pypi.python.org/pypi/Bottleneck",
+                license="Simplified BSD",
                 classifiers=CLASSIFIERS,
-                author=AUTHOR,
-                author_email=AUTHOR_EMAIL,
-                platforms=PLATFORMS,
-                version=VERSION,
-                packages=PACKAGES,
-                package_data=PACKAGE_DATA,
-                requires=REQUIRES,
+                platforms="OS Independent",
+                version=get_version_str(),
+                packages=find_packages(),
+                package_data={'bottleneck': ['LICENSE']},
+                requires=['numpy'],
                 install_requires=['numpy'])
 
 
 if not(len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
        sys.argv[1] in ('--help-commands', 'egg_info', '--version', 'clean',
                        'build_sphinx'))):
+    # build bottleneck
     metadata['ext_modules'] = prepare_modules()
 elif sys.argv[1] == 'build_sphinx':
     # create intro.rst (from readme file) for sphinx manual
