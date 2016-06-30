de224e1c4c7bf4cba19e1ed9f2cea7ed84a6553a
Keith Goodman <kwgoodman@gmail.com>
2016-06-30 14:31:12 -0700

    handle runtime errors in slow.c

--- bottleneck/src/slow.c
+++ bottleneck/src/slow.c
@@ -1,6 +1,6 @@
 #include <Python.h>
 
-PyObject *slow_module = NULL;
+static PyObject *slow_module = NULL;
 
 
 PyObject *
@@ -10,29 +10,36 @@ slow(char *name, PyObject *args, PyObject *kwds)
     PyObject *out = NULL;
 
     if (slow_module == NULL) {
+        /* bottleneck.slow has not been imported during the current
+         * python session. Only import it once per session to save time*/
         slow_module = PyImport_ImportModule("bottleneck.slow");
+        if (slow_module == NULL) {
+            PyErr_SetString(PyExc_RuntimeError,
+                            "Cannot import bottleneck.slow");
+            return NULL;
+        }
     }
 
-    if (slow_module != NULL) {
-        func = PyObject_GetAttrString(slow_module, name);
-        if (func && PyCallable_Check(func)) {
-            out = PyObject_Call(func, args, kwds);
-            if (out == NULL) {
-                Py_DECREF(func);
-                return NULL;
-            }
-        }
-        else {
-            if (PyErr_Occurred())
-                PyErr_Print();
-        }
-        Py_XDECREF(func);
-    }
-    else {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", name);
+    func = PyObject_GetAttrString(slow_module, name);
+    if (func == NULL) {
+        PyErr_Format(PyExc_RuntimeError,
+                     "Cannot import %s from bottleneck.slow", name);
         return NULL;
     }
+    if (PyCallable_Check(func)) {
+        out = PyObject_Call(func, args, kwds);
+        if (out == NULL) {
+            Py_XDECREF(func);
+            return NULL;
+        }
+    }
+    else {
+        Py_XDECREF(func);
+        PyErr_Format(PyExc_RuntimeError,
+                     "bottleneck.slow.%s is not callable", name);
+        return NULL;
+    }
+    Py_XDECREF(func);
 
     return out;
 }
