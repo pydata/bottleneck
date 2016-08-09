c3dd4b29490984519f73c2bf320d0a9536c4f849
Keith Goodman <kwgoodman@gmail.com>
2016-08-09 14:15:05 -0700

    bug in unit tests; sorted input array does not...
    ..catch possible changes made to input array
    by a function like median.

--- bottleneck/tests/input_modifcation_test.py
+++ bottleneck/tests/input_modifcation_test.py
@@ -11,27 +11,28 @@ DTYPES = [np.float64, np.float32, np.int64, np.int32]
 nan = np.nan
 
 
-def arrays(dtypes=DTYPES, nans=True):
+def arrays(dtypes):
     "Iterator that yield arrays to use for unit testing."
     ss = {}
     ss[1] = {'size':  4, 'shapes': [(4,)]}
     ss[2] = {'size':  6, 'shapes': [(2, 3)]}
     ss[3] = {'size':  6, 'shapes': [(1, 2, 3)]}
-    for ndim in ss:
-        size = ss[ndim]['size']
-        shapes = ss[ndim]['shapes']
-        for dtype in dtypes:
-            a = np.arange(size, dtype=dtype)
-            for shape in shapes:
-                a = a.reshape(shape)
-                yield a
-            if issubclass(a.dtype.type, np.inexact):
-                if nans:
-                    for i in range(a.size):
-                        a.flat[i] = np.nan
-                        yield a
-                for i in range(a.size):
-                    a.flat[i] = np.inf
+    for seed in (1, 2):
+        rs = np.random.RandomState(seed)
+        for ndim in ss:
+            size = ss[ndim]['size']
+            shapes = ss[ndim]['shapes']
+            for dtype in dtypes:
+                a = np.arange(size, dtype=dtype)
+                if issubclass(a.dtype.type, np.inexact):
+                    idx = rs.rand(*a.shape) < 0.2
+                    a[idx] = np.inf
+                    idx = rs.rand(*a.shape) < 0.2
+                    a[idx] = np.nan
+                    idx = rs.rand(*a.shape) < 0.2
+                    a[idx] *= -1
+                for shape in shapes:
+                    a = a.reshape(shape)
                     yield a
 
 
@@ -39,7 +40,7 @@ def unit_maker(func, nans=True):
     "Test that bn.xxx gives the same output as np.xxx."
     msg = "\nInput array modifed by %s.\n\n"
     msg += "input array before:\n%s\nafter:\n%s\n"
-    for i, arr in enumerate(arrays(nans=nans)):
+    for i, arr in enumerate(arrays(DTYPES)):
         for axis in list(range(-arr.ndim, arr.ndim)) + [None]:
             with np.errstate(invalid='ignore'):
                 arr1 = arr.copy()
