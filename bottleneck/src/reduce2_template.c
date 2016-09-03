ab5bcc4e8bf9c5bb488fa25b001fb55e78cb9a66
Keith Goodman <kwgoodman@gmail.com>
2016-09-03 09:23:47 -0700

    begin redesign of bottleneck iterators

--- bottleneck/src/reduce2_template.c
+++ bottleneck/src/reduce2_template.c
@@ -136,6 +136,62 @@ reducer(char *name,
 
 /* nansum ---------------------------------------------------------------- */
 
+struct _iter {
+    Py_ssize_t length;
+    int        ndim_m2;
+    int        axis;
+    npy_intp   i;
+    npy_intp   index;
+    npy_intp   size;
+    npy_intp   indices[NPY_MAXDIMS];
+    npy_intp   strides[NPY_MAXDIMS];
+    npy_intp   shape_m1[NPY_MAXDIMS];
+    char      *p;
+};
+typedef struct _iter iter;
+
+static iter *
+new_iter(PyArrayObject *a, int axis, int ndim, Py_ssize_t length)
+{
+    int i, j = 0;
+    iter *it = malloc(sizeof(iter));
+
+    it->length = length;
+    it->ndim_m2 = ndim - 2;
+    it->axis = axis;
+    it->index = 0;
+    it->size = PyArray_SIZE(a);
+    it->p = PyArray_DATA(a);
+
+    if (length != 0) it->size /= length;
+
+    memset(it->indices, 0, (ndim - 1) * sizeof(npy_intp));
+    for (i = 0; i < ndim; i++) {
+        if (i == axis) continue;
+        it->strides[j] = PyArray_STRIDE(a, i);
+        it->shape_m1[j] = PyArray_DIM(a, i) - 1;
+        j++;
+    }
+
+    return it;
+}
+
+#define NEXT99 \
+    for (it->i = it->ndim_m2; it->i > -1; it->i--) { \
+        if (it->indices[it->i] < it->shape_m1[it->i]) { \
+            it->p += it->strides[it->i]; \
+            it->indices[it->i]++; \
+            break; \
+        } \
+        it->p -= it->indices[it->i] * it->strides[it->i]; \
+        it->indices[it->i] = 0; \
+    } \
+    it->index++;
+
+#define  WHILE99          while (it->index < it->size)
+#define  FOR99            for (it->i = 0; it->i < it->length; it->i++)
+#define  AI99(dt)         *(dt*)(it->p + it->i * stride)
+
 /* dtype = [['float64'], ['float32']] */
 REDUCE_ALL(nansum, DTYPE0)
 {
@@ -155,25 +211,26 @@ REDUCE_ALL(nansum, DTYPE0)
 
 REDUCE_ONE(nansum, DTYPE0)
 {
+    iter *it = new_iter(a, axis, ndim, length);
     Y_INIT(NPY_DTYPE0, npy_DTYPE0)
-    INIT
     BN_BEGIN_ALLOW_THREADS
     npy_DTYPE0 ai, asum;
     if (length == 0) {
         Py_ssize_t length = PyArray_SIZE((PyArrayObject *)y);
-        FOR YI = 0;
+        for (it->i = 0; it->i < length; it->i++) YI = 0;
     }
     else {
-        WHILE {
+        WHILE99 {
             asum = 0;
-            FOR {
-                ai = AI(npy_DTYPE0);
+            FOR99 {
+                ai = AI99(npy_DTYPE0);
                 if (ai == ai) asum += ai;
             }
             YI = asum;
-            NEXT
+            NEXT99
         }
     }
+    free(it);
     BN_END_ALLOW_THREADS
     return y;
 }
