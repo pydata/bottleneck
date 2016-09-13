4bfee1f4454c88d866feba0512b265b1c7d2ac03
Keith Goodman <kwgoodman@gmail.com>
2016-09-13 11:44:08 -0700

    move slow() function to bottleneck.h
    see issue #134

--- bottleneck/src/bottleneck.h
+++ bottleneck/src/bottleneck.h
@@ -4,7 +4,6 @@
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_11_API_VERSION
 #include <numpy/arrayobject.h>
-#include "slow.h"
 
 /* THREADS=1 releases the GIL but increases function call
  * overhead. THREADS=0 does not release the GIL but keeps
@@ -149,5 +148,50 @@ BN_INLINE static float __bn_nanf(void)
         } \
         WIRTH(dtype) \
     }
+
+/* slow ------------------------------------------------------------------ */
+
+static PyObject *slow_module = NULL;
+
+static PyObject *
+slow(char *name, PyObject *args, PyObject *kwds)
+{
+    PyObject *func = NULL;
+    PyObject *out = NULL;
+
+    if (slow_module == NULL) {
+        /* bottleneck.slow has not been imported during the current
+         * python session. Only import it once per session to save time */
+        slow_module = PyImport_ImportModule("bottleneck.slow");
+        if (slow_module == NULL) {
+            PyErr_SetString(PyExc_RuntimeError,
+                            "Cannot import bottleneck.slow");
+            return NULL;
+        }
+    }
+
+    func = PyObject_GetAttrString(slow_module, name);
+    if (func == NULL) {
+        PyErr_Format(PyExc_RuntimeError,
+                     "Cannot import %s from bottleneck.slow", name);
+        return NULL;
+    }
+    if (PyCallable_Check(func)) {
+        out = PyObject_Call(func, args, kwds);
+        if (out == NULL) {
+            Py_XDECREF(func);
+            return NULL;
+        }
+    }
+    else {
+        Py_XDECREF(func);
+        PyErr_Format(PyExc_RuntimeError,
+                     "bottleneck.slow.%s is not callable", name);
+        return NULL;
+    }
+    Py_XDECREF(func);
+
+    return out;
+}
 
 #endif /* BOTTLENECK_H */
