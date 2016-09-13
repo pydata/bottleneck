0a573de714a5e2d789afd14435811e9546e6e821
Keith Goodman <kwgoodman@gmail.com>
2016-09-13 10:00:25 -0700

    add code comment

--- bottleneck/src/slow.h
+++ bottleneck/src/slow.h
@@ -1,9 +1,15 @@
 #include <Python.h>
 
+/*
+ * If I put the slow() function in a slow.c file then I have to add slow.c
+ * to each module in setup.py (that's not so bad) but it doubles the output
+ * (to stdout) of the build process (I didn't like that). So I put the
+ * function in this header. And then wrote this overly long justification.
+ */
+
 static PyObject *slow_module = NULL;
 
-
-PyObject *
+static PyObject *
 slow(char *name, PyObject *args, PyObject *kwds)
 {
     PyObject *func = NULL;
@@ -11,7 +17,7 @@ slow(char *name, PyObject *args, PyObject *kwds)
 
     if (slow_module == NULL) {
         /* bottleneck.slow has not been imported during the current
-         * python session. Only import it once per session to save time*/
+         * python session. Only import it once per session to save time */
         slow_module = PyImport_ImportModule("bottleneck.slow");
         if (slow_module == NULL) {
             PyErr_SetString(PyExc_RuntimeError,
