984eb0fa54e765cf3ae66ea32078cd040ce609cd
Keith Goodman <kwgoodman@gmail.com>
2016-09-21 11:22:56 -0700

    refactor bench_detailed

--- bottleneck/benchmark/bench_detailed.py
+++ bottleneck/benchmark/bench_detailed.py
@@ -12,203 +12,100 @@ def bench_detailed(function='nansum'):
     tab = '    '
 
     # Header
-    print('Bottleneck performance benchmark of C rewrite')
+    print('%s benchmark' % function)
     print("%sBottleneck %s; Numpy %s" % (tab, bn.__version__, np.__version__))
     print("%sSpeed is Bottleneck Cython time divided by Bottleneck C time"
           % tab)
     print("")
 
-    print("   Speed  Call                     Input")
+    print("   Speed  Call                     Array")
     suite = benchsuite(function)
     for test in suite:
         name = test["name"]
-        speed = timer(test['statements'], test['setup'])
-        print("%7.2f   %s   %s" % (speed, name[0].ljust(22), name[1]))
+        speed = timer(test['statements'], test['setup'], test['repeat'])
+        print("%8.1f  %s   %s" % (speed, name[0].ljust(22), name[1]))
 
 
-def timer(statements, setup):
+def timer(statements, setup, repeat):
     if len(statements) != 2:
         raise ValueError("Two statements needed.")
     with np.errstate(invalid='ignore'):
-        t0 = autotimeit(statements[0], setup, repeat=6)
-        t1 = autotimeit(statements[1], setup, repeat=6)
+        t0 = autotimeit(statements[0], setup, repeat=repeat)
+        t1 = autotimeit(statements[1], setup, repeat=repeat)
     speed = t1 / t0
     return speed
 
 
 def benchsuite(function):
 
-    is_reduce_like = (function in bn.get_functions('reduce', as_string=True) or
-                      function in ['rankdata', 'nanrankdata'])
-    is_move_like = (function in bn.get_functions('move', as_string=True) or
-                    function in ['partsort', 'argpartsort', 'push'])
+    repeat_array_sig = [
 
-    if is_reduce_like:
-        suite = suite_reduce(function)
-    elif is_move_like:
-        suite = suite_move(function)
+     (10, "rand(1)",             "(a)",    "(a, 1)",         "(a, np.nan, 0)"),
+     (10, "rand(10)",            "(a)",    "(a, 2)",         "(a, np.nan, 0)"),
+     (6,  "rand(100)",           "(a)",    "(a, 20)",        "(a, np.nan, 0)"),
+     (3,  "rand(1000)",          "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
+     (2,  "rand(1000000)",       "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
+
+     (6,  "rand(10, 10)",        "(a)",    "(a, 2)",         "(a, np.nan, 0)"),
+     (3,  "rand(100, 100)",      "(a)",    "(a, 20)",        "(a, np.nan, 0)"),
+     (2,  "rand(1000, 1000)",    "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
+
+     (6,  "rand(10, 10)",        "(a, 1)", None,             None),
+     (3,  "rand(100, 100)",      "(a, 1)", None,             None),
+     (3,  "rand(1000, 1000)",    "(a, 1)", None,             None),
+     (2,  "rand(100000, 2)",     "(a, 1)", "(a, 2)",         None),
+
+     (6,  "rand(10, 10)",        "(a, 0)", None,             None),
+     (3,  "rand(100, 100)",      "(a, 0)", None,             None),
+     (2,  "rand(1000, 1000)",    "(a, 0)", None,             None),
+
+     (2,  "rand(100, 100, 100)", "(a, 0)", None,             None),
+     (2,  "rand(100, 100, 100)", "(a, 1)", None,             None),
+     (2,  "rand(100, 100, 100)", "(a, 2)", "(a, 20)",        "(a, np.nan, 0)"),
+
+     # TODO replace last None with (a, 0, 2) after removing cython
+     (10, "array(1)",            "(a)",    None,             None),
+
+     ]
+
+    setup = """
+        import numpy as np
+        from bottleneck import %s
+        from bottleneck import %s2 as %s_c
+        from numpy.random import rand
+        from numpy import array
+        a=%s
+    """
+    setup = '\n'.join([s.strip() for s in setup.split('\n')])
+
+    # what kind of function signature do we need to use?
+    if function in bn.get_functions('reduce', as_string=True):
+        index = 0
+    elif function in bn.get_functions('move', as_string=True):
+        index = 1
+    elif function in ['rankdata', 'nanrankdata']:
+        index = 0
+    elif function in ['partsort', 'argpartsort', 'push']:
+        index = 1
     elif function == 'replace':
-        suite = suite_replace(function)
+        index = 2
     else:
         raise ValueError("`function` (%s) not recognized" % function)
 
-    # Strip leading spaces from setup code
-    for i, run in enumerate(suite):
-        t = run['setup']
-        t = '\n'.join([z.strip() for z in t.split('\n')])
-        suite[i]['setup'] = t
-
-    return suite
-
-
-def suite_reduce(function):
-
-    setup = """
-        import numpy as np
-        from bottleneck import %s
-        from bottleneck import %s2 as %s_c
-        from numpy import array
-        from numpy.random import rand
-        a=%s
-    """
-
-    arrays_0d = ["array(1)"]
-
-    arrays_1d = [
-                 "rand(1)",
-                 "rand(10)",
-                 "rand(100)",
-                 "rand(1000)",
-                 "rand(1000000)",
-                 "rand(1).astype(np.float16)",
-                 ]
-
-    arrays_2d = [
-                 "rand(1, 1)",
-                 "rand(10, 10)",
-                 "rand(100, 100)",
-                 "rand(1000, 1000)",
-                 "rand(1000000, 2)",
-                 "rand(2, 1000000)",
-                 "rand(4, 1)[::2]",
-                 "rand(200, 100)[::2]",
-                 "rand(2000000, 2)[::2]",
-                 "rand(2, 2000000)[:, ::2]",
-                 ]
-
-    arrays_3d = ["rand(100, 100, 100)"]
-
-    sig_array = []
-    for arr in arrays_1d:
-        sig_array.append(("%s%s(a)", arr))
-    for arr in arrays_2d:
-        sig_array.append(("%s%s(a)", arr))
-    for arr in arrays_2d:
-        sig_array.append(("%s%s(a, 1)", arr))
-    for arr in arrays_3d:
-        sig_array.append(("%s%s(a, 0)", arr))
-        sig_array.append(("%s%s(a, 1)", arr))
-        sig_array.append(("%s%s(a, 2)", arr))
-    for arr in arrays_0d:
-        sig_array.append(("%s%s(a)", arr))
-
+    # create benchmark suite
     f = function
     suite = []
-    for signature, array in sig_array:
+    for instructions in repeat_array_sig:
+        signature = instructions[index + 2]
+        if signature is None:
+            continue
+        repeat = instructions[0]
+        array = instructions[1]
         run = {}
-        run['name'] = [signature % (f, ''), array]
-        run['statements'] = [signature % (f, "_c"), signature % (f, "")]
+        run['name'] = [f + signature, array]
+        run['statements'] = [f + "_c" + signature, f + signature]
         run['setup'] = setup % (f, f, f, array)
-        suite.append(run)
-
-    return suite
-
-
-def suite_replace(function):
-
-    setup = """
-        import numpy as np
-        from bottleneck import %s
-        from bottleneck import %s2 as %s_c
-        from numpy import array
-        from numpy.random import rand
-        a=%s
-    """
-
-    arrays_1d = [
-                 "rand(1)",
-                 "rand(10)",
-                 "rand(100)",
-                 "rand(1000)",
-                 "rand(1000000)",
-                 "rand(1).astype(np.float16)",
-                 ]
-
-    arrays_2d = [
-                 "rand(1, 1)",
-                 "rand(10, 10)",
-                 "rand(100, 100)",
-                 "rand(1000, 1000)",
-                 "rand(1000000, 2)",
-                 "rand(2, 1000000)",
-                 "rand(4, 1)[::2]",
-                 "rand(200, 100)[::2]",
-                 "rand(2000000, 2)[::2]",
-                 "rand(2, 2000000)[:, ::2]",
-                 ]
-
-    arrays_3d = ["rand(100, 100, 100)"]
-
-    sig_array = []
-    for arr in arrays_1d:
-        sig_array.append(("%s%s(a, np.nan, 0)", arr))
-    for arr in arrays_2d:
-        sig_array.append(("%s%s(a, np.nan, 0)", arr))
-    for arr in arrays_2d:
-        sig_array.append(("%s%s(a, np.nan, 0)", arr))
-    for arr in arrays_3d:
-        sig_array.append(("%s%s(a, np.nan, 0)", arr))
-
-    f = function
-    suite = []
-    for signature, array in sig_array:
-        run = {}
-        run['name'] = [signature % (f, ''), array]
-        run['statements'] = [signature % (f, "_c"), signature % (f, "")]
-        run['setup'] = setup % (f, f, f, array)
-        suite.append(run)
-
-    return suite
-
-
-def suite_move(function):
-
-    setup = """
-        import numpy as np
-        from bottleneck import %s
-        from bottleneck import %s2 as %s_c
-        from numpy.random import rand
-        a=%s
-    """
-
-    sig_array = [
-                 ("%s%s(a, 2)",   "rand(2)"),
-                 ("%s%s(a, 10)",  "rand(100)"),
-                 ("%s%s(a, 100)", "rand(1000)"),
-                 ("%s%s(a, 2)",   "rand(1000*1000)"),
-                 ("%s%s(a, 100)", "rand(1000*1000)"),
-                 ("%s%s(a, 2)",   "rand(1000, 1000)"),
-                 ("%s%s(a, 2)",   "rand(1000, 2)"),
-                 ("%s%s(a, 2)",   "rand(1000, 1000, 2)"),
-                ]
-
-    f = function
-    suite = []
-    for signature, array in sig_array:
-        run = {}
-        run['name'] = [signature % (f, ''), array]
-        run['statements'] = [signature % (f, "_c"), signature % (f, "")]
-        run['setup'] = setup % (f, f, f, array)
+        run['repeat'] = repeat
         suite.append(run)
 
     return suite
