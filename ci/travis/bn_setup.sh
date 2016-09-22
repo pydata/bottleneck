59f5c21f19e7c67945277d3931e8fd12db54f10b
Keith Goodman <kwgoodman@gmail.com>
2016-09-22 09:17:52 -0700

    remove cython from travis

--- ci/travis/bn_setup.sh
+++ ci/travis/bn_setup.sh
@@ -4,12 +4,6 @@ set -ev # exit on first error, print commands
 
 if [ "${TEST_RUN}" = "style" ]; then
     flake8 bottleneck
-elif [ "${TEST_RUN}" = "cythonless" ]; then
-    python setup.py sdist
-    conda uninstall -n "${TEST_NAME}" cython
-    ARCHIVE=`ls dist/*.tar.gz`
-    pip install --verbose "${ARCHIVE[0]}"
-    python "tools/test-installed-bottleneck.py"
 elif [ "${TEST_RUN}" = "sdist" ]; then
     python setup.py sdist
     ARCHIVE=`ls dist/*.tar.gz`
