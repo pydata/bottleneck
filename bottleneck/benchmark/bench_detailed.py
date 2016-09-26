5b64f8124f85037da692a7c3fd6d9b099c1d5990
Keith Goodman <kwgoodman@gmail.com>
2016-09-26 13:15:01 -0700

    refactor and add more input arrays: bench_detailed

--- bottleneck/benchmark/bench_detailed.py
+++ bottleneck/benchmark/bench_detailed.py
@@ -41,12 +41,12 @@ def bench_detailed(function='nansum', fraction_nan=0.0):
               % (tab, fraction_nan * 100))
     print("")
 
-    print("   Speed  Call                     Array")
+    print("   Speed  Call                          Array")
     suite = benchsuite(function, fraction_nan)
     for test in suite:
         name = test["name"]
         speed = timer(test['statements'], test['setup'], test['repeat'])
-        print("%8.1f  %s   %s" % (speed, name[0].ljust(22), name[1]))
+        print("%8.1f  %s   %s" % (speed, name[0].ljust(27), name[1]))
 
 
 def timer(statements, setup, repeat):
@@ -61,46 +61,17 @@ def timer(statements, setup, repeat):
 
 def benchsuite(function, fraction_nan):
 
-    repeat_array_sig = [
-
-     (10, "rand(1)",             "(a)",    "(a, 1)",         "(a, np.nan, 0)"),
-     (10, "rand(10)",            "(a)",    "(a, 2)",         "(a, np.nan, 0)"),
-     (6,  "rand(100)",           "(a)",    "(a, 20)",        "(a, np.nan, 0)"),
-     (3,  "rand(1000)",          "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
-     (2,  "rand(1000000)",       "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
-
-     (6,  "rand(10, 10)",        "(a)",    "(a, 2)",         "(a, np.nan, 0)"),
-     (3,  "rand(100, 100)",      "(a)",    "(a, 20)",        "(a, np.nan, 0)"),
-     (2,  "rand(1000, 1000)",    "(a)",    "(a, 200)",       "(a, np.nan, 0)"),
-
-     (6,  "rand(10, 10)",        "(a, 1)", None,             None),
-     (3,  "rand(100, 100)",      "(a, 1)", None,             None),
-     (3,  "rand(1000, 1000)",    "(a, 1)", None,             None),
-     (2,  "rand(100000, 2)",     "(a, 1)", "(a, 2)",         None),
-
-     (6,  "rand(10, 10)",        "(a, 0)", None,             None),
-     (3,  "rand(100, 100)",      "(a, 0)", None,             None),
-     (2,  "rand(1000, 1000)",    "(a, 0)", None,             None),
-
-     (2,  "rand(100, 100, 100)", "(a, 0)", None,             None),
-     (2,  "rand(100, 100, 100)", "(a, 1)", None,             None),
-     (2,  "rand(100, 100, 100)", "(a, 2)", "(a, 20)",        "(a, np.nan, 0)"),
-
-     (10, "array(1.0)",          "(a)",    None,             "(a, 0, 2)"),
-
-     ]
-
     # what kind of function signature do we need to use?
     if function in bn.get_functions('reduce', as_string=True):
         index = 0
-    elif function in bn.get_functions('move', as_string=True):
-        index = 1
     elif function in ['rankdata', 'nanrankdata']:
         index = 0
-    elif function in ['partition', 'argpartition', 'push']:
+    elif function in bn.get_functions('move', as_string=True):
         index = 1
-    elif function == 'replace':
+    elif function in ['partition', 'argpartition', 'push']:
         index = 2
+    elif function == 'replace':
+        index = 3
     else:
         raise ValueError("`function` (%s) not recognized" % function)
 
@@ -119,14 +90,15 @@ def benchsuite(function, fraction_nan):
     setup = '\n'.join([s.strip() for s in setup.split('\n')])
 
     # create benchmark suite
+    instructions = get_suite_instructions()
     f = function
     suite = []
-    for instructions in repeat_array_sig:
-        signature = instructions[index + 2]
+    for instruction in instructions:
+        signature = instruction[index + 1]
         if signature is None:
             continue
-        repeat = instructions[0]
-        array = instructions[1]
+        array = instruction[0]
+        repeat = instruction[-1]
         run = {}
         run['name'] = [f + signature, array]
         run['statements'] = ["bn_fn" + signature, "sl_fn" + signature]
@@ -136,3 +108,149 @@ def benchsuite(function, fraction_nan):
         suite.append(run)
 
     return suite
+
+
+def get_suite_instructions():
+
+    instructions = [
+
+        # 1d input array
+        ("rand(1)",
+         "(a)",              # reduce + (nan)rankdata
+         "(a, 1)",           # move
+         "(a, 0)",           # (arg)partition
+         "(a, np.nan, 0)",   # replace
+         10),
+
+        ("rand(10)",
+         "(a)",
+         "(a, 2)",
+         "(a, 2)",
+         "(a, np.nan, 0)",
+         10),
+
+        ("rand(100)",
+         "(a)",
+         "(a, 20)",
+         "(a, 20)",
+         "(a, np.nan, 0)",
+         6),
+
+        ("rand(1000)",
+         "(a)",
+         "(a, 200)",
+         "(a, 200)",
+         "(a, np.nan, 0)",
+         3),
+
+        ("rand(1000000)",
+         "(a)",
+         "(a, 200)",
+         "(a, 200)",
+         "(a, np.nan, 0)",
+         2),
+
+        # 2d input array
+        ("rand(10, 10)",
+         "(a)",
+         "(a, 2)",
+         "(a, 2)",
+         "(a, np.nan, 0)",
+         6),
+
+        ("rand(100, 100)",
+         "(a)",
+         "(a, 20)",
+         "(a, 20)",
+         "(a, np.nan, 0)",
+         3),
+
+        ("rand(1000, 1000)",
+         "(a)",
+         "(a, 200)",
+         "(a, 200)",
+         "(a, np.nan, 0)",
+         2),
+
+        ("rand(10, 10)",
+         "(a, 1)",
+         None,
+         None,
+         None,
+         6),
+
+        ("rand(100, 100)",
+         "(a, 1)",
+         None,
+         None,
+         None,
+         3),
+
+        ("rand(1000, 1000)",
+         "(a, 1)",
+         None,
+         None,
+         None,
+         2),
+
+        ("rand(100000, 2)",
+         "(a, 1)",
+         "(a, 1)",
+         "(a, 1)",
+         None,
+         2),
+
+        ("rand(10, 10)",
+         "(a, 0)",
+         None,
+         None,
+         None,
+         6),
+
+        ("rand(100, 100)",
+         "(a, 0)",
+         "(a, 20, axis=0)",
+         None,
+         None,
+         3),
+
+        ("rand(1000, 1000)",
+         "(a, 0)",
+         "(a, 200, axis=0)",
+         None,
+         None,
+         2),
+
+        # 3d input array
+        ("rand(100, 100, 100)",
+         "(a, 0)",
+         "(a, 20, axis=0)",
+         "(a, 20, axis=0)",
+         None,
+         2),
+
+        ("rand(100, 100, 100)",
+         "(a, 1)",
+         "(a, 20, axis=1)",
+         "(a, 20, axis=1)",
+         None,
+         2),
+
+        ("rand(100, 100, 100)",
+         "(a, 2)",
+         "(a, 20, axis=2)",
+         "(a, 20, axis=2)",
+         "(a, np.nan, 0)",
+         2),
+
+        # 0d input array
+        ("array(1.0)",
+         "(a)",
+         None,
+         None,
+         "(a, 0, 2)",
+         10),
+
+     ]
+
+    return instructions
