f51ed862e619dcff7b7b3691e58d5fc6ead4076b
Christopher Whelan <topherwhelan@gmail.com>
2019-09-14 21:36:05 -0700

    BLD: Add coverage

--- tools/travis/bn_setup.sh
+++ tools/travis/bn_setup.sh
@@ -2,23 +2,36 @@
 
 set -ev # exit on first error, print commands
 
-if [ "${TEST_RUN}" = "style" ]; then
+COVERAGE=""
+if [ "${TEST_RUN}" == "coverage" ]; then
+    COVERAGE="--coverage"
+    export CFLAGS="-fprofile-arcs -ftest-coverage"
+    export LDFLAGS="-fprofile-arcs"
+fi
+
+if [ "${TEST_RUN}" == "style" ]; then
     flake8
     black . --check --exclude "(build/|dist/|\.git/|\.mypy_cache/|\.tox/|\.venv/\.asv/|env|\.eggs)"
 else
-    if [ "${TEST_RUN}" = "sdist" ]; then
+    if [ "${TEST_RUN}" == "sdist" ]; then
         python setup.py sdist
         ARCHIVE=`ls dist/*.tar.gz`
         pip install "${ARCHIVE[0]}"
-    else
+    elif [ "${TEST_RUN}" != "coverage" ]; then
+        # CFLAGS gets ignored by PEP 518, so do coverage from inplace build
         pip install "."
     fi
     python setup.py build_ext --inplace
-    set +e
-    if [ "${TEST_RUN}" = "doc" ]; then
-	make doc
+    if [ "${TEST_RUN}" == "doc" ]; then
+        make doc
+    elif [ "${TEST_RUN}" == "coverage" ]; then
+        py.test --cov=bottleneck --cov-branch
+        pushd build/temp*
+        find . -iname "*.gc*" -exec mv {} ../../{} \;
+        popd
+        bash <(curl -s https://codecov.io/bash)
     else
-	# Workaround for https://github.com/travis-ci/travis-ci/issues/6522
-	python "tools/test-installed-bottleneck.py"
+        # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
+        python "tools/test-installed-bottleneck.py"
     fi
 fi
