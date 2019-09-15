46ca7e39fccb9b1181119de4c2d1175c5cbb46e5
Christopher Whelan <topherwhelan@gmail.com>
2019-09-14 21:36:05 -0700

    BLD: Add coverage

--- tools/travis/bn_setup.sh
+++ tools/travis/bn_setup.sh
@@ -2,20 +2,37 @@
 
 set -ev # exit on first error, print commands
 
-if [ "${TEST_RUN}" = "style" ]; then
+COVERAGE=""
+if [ "${TEST_RUN}" == "coverage" ]; then
+    COVERAGE="--coverage"
+    export CFLAGS="-fprofile-arcs -ftest-coverage"
+    export LDFLAGS="-fprofile-arcs"
+fi
+
+if [ "${TEST_RUN}" == "style" ]; then
     flake8
 else
-    if [ "${TEST_RUN}" = "sdist" ]; then
+    if [ "${TEST_RUN}" == "sdist" ]; then
         python setup.py sdist
         ARCHIVE=`ls dist/*.tar.gz`
         pip install "${ARCHIVE[0]}"
-    else
+    elif [ "${TEST_RUN}" != "coverage" ]; then
+	# CFLAGS gets ignored by PEP 518, so do coverage from inplace build
         pip install "."
     fi
     python setup.py build_ext --inplace
-    set +e
-    if [ "${TEST_RUN}" = "doc" ]; then
+    if [ "${TEST_RUN}" == "doc" ]; then
 	make doc
+    elif [ "${TEST_RUN}" == "coverage" ]; then
+	py.test --cov=bottleneck --cov-branch
+	find build -iname "*.gc*" -exec mv {} . \;
+#	cd build/temp.linux-x86_64-3.7/
+#	cp -r bottleneck ../..
+	#	cd ../..
+	echo $(ls)
+	echo $(ls bottleneck)
+	echo $(ls bottleneck/src)
+	bash <(curl -s https://codecov.io/bash)
     else
 	# Workaround for https://github.com/travis-ci/travis-ci/issues/6522
 	python "tools/test-installed-bottleneck.py"
