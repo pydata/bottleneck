042632b5686b36cd791afa36612fe53a9214dc99
Christopher Whelan <topherwhelan@gmail.com>
2019-11-09 13:58:19 -0800

    DOC: update PyPI classifiers

--- setup.py
+++ setup.py
@@ -150,14 +150,20 @@ def get_long_description():
 
 
 CLASSIFIERS = [
-    "Development Status :: 4 - Beta",
+    "Development Status :: 5 - Production/Stable",
     "Environment :: Console",
     "Intended Audience :: Science/Research",
+    "Intended Audience :: Financial and Insurance Industry",
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: C",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering",
 ]
 
