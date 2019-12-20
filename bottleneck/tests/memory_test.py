59f4d3225300e7ffe078684cbf9844952bbc466f
Christopher Whelan <topherwhelan@gmail.com>
2019-12-19 22:15:55 -0800

    TST: reduce flakiness of memory leak test

--- bottleneck/tests/memory_test.py
+++ bottleneck/tests/memory_test.py
@@ -12,24 +12,32 @@ def test_memory_leak():
 
     arr = np.arange(1).reshape((1, 1))
 
-    starting = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    n_attempts = 3
+    results = []
 
-    for i in range(1000):
-        for axis in [None, 0, 1]:
-            bn.nansum(arr, axis=axis)
-            bn.nanargmax(arr, axis=axis)
-            bn.nanargmin(arr, axis=axis)
-            bn.nanmedian(arr, axis=axis)
-            bn.nansum(arr, axis=axis)
-            bn.nanmean(arr, axis=axis)
-            bn.nanmin(arr, axis=axis)
-            bn.nanmax(arr, axis=axis)
-            bn.nanvar(arr, axis=axis)
+    for _ in range(n_attempts):
+        starting = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-    ending = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        for _ in range(1000):
+            for axis in [None, 0, 1]:
+                bn.nansum(arr, axis=axis)
+                bn.nanargmax(arr, axis=axis)
+                bn.nanargmin(arr, axis=axis)
+                bn.nanmedian(arr, axis=axis)
+                bn.nansum(arr, axis=axis)
+                bn.nanmean(arr, axis=axis)
+                bn.nanmin(arr, axis=axis)
+                bn.nanmax(arr, axis=axis)
+                bn.nanvar(arr, axis=axis)
 
-    diff = ending - starting
-    diff_bytes = diff * resource.getpagesize()
-    print(diff_bytes)
-    # For 1.3.0 release, this had value of ~100kB
-    assert diff_bytes == 0
+        ending = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+        diff = ending - starting
+        diff_bytes = diff * resource.getpagesize()
+        # For 1.3.0 release, this had value of ~100kB
+        if diff_bytes:
+            results.append(diff_bytes)
+        else:
+            break
+
+    assert len(results) < n_attempts
