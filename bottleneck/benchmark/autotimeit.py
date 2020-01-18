37110cf7e7a7ecd576b536d76468860418e232ad
Christopher Whelan <topherwhelan@gmail.com>
2020-01-18 00:33:36 -0800

    CLN: add typing to autotimeit.py

--- bottleneck/benchmark/autotimeit.py
+++ bottleneck/benchmark/autotimeit.py
@@ -1,16 +1,19 @@
 import timeit
+from typing import Tuple
 
 
-def autotimeit(stmt, setup="pass", repeat=3, mintime=0.2):
+def autotimeit(
+    stmt: str, setup: str = "pass", repeat: int = 3, mintime: float = 0.2
+) -> float:
     timer = timeit.Timer(stmt, setup)
     number, time1 = autoscaler(timer, mintime)
     time2 = timer.repeat(repeat=repeat - 1, number=number)
     return min(time2 + [time1]) / number
 
 
-def autoscaler(timer, mintime):
+def autoscaler(timer: timeit.Timer, mintime: float) -> Tuple[int, float]:
     number = 1
-    for i in range(12):
+    for _ in range(12):
         time = timer.timeit(number)
         if time > mintime:
             return number, time
