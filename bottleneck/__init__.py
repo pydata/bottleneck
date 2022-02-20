d4e84944c03b63f06b8a765039b1add3d8bb07b4
Ruben Di Battista <rubendibattista@gmail.com>
2022-02-20 17:12:42 +0100

    MNT: Move CI to Github Actions

--- bottleneck/__init__.py
+++ bottleneck/__init__.py
@@ -1,50 +1,16 @@
-try:
-    from .reduce import (
-        nansum,
-        nanmean,
-        nanstd,
-        nanvar,
-        nanmin,
-        nanmax,
-        median,
-        nanmedian,
-        ss,
-        nanargmin,
-        nanargmax,
-        anynan,
-        allnan,
-    )
-    from .nonreduce import replace
-    from .nonreduce_axis import partition, argpartition, rankdata, nanrankdata, push
-    from .move import (
-        move_sum,
-        move_mean,
-        move_std,
-        move_var,
-        move_min,
-        move_max,
-        move_argmin,
-        move_argmax,
-        move_median,
-        move_rank,
-    )
-
-    from . import slow
-
-except ImportError:
-    raise ImportError(
-        "bottleneck modules failed to import, likely due to a "
-        "mismatch in NumPy versions. Either upgrade numpy to "
-        "1.16+ or install with:\n\t"
-        "pip install --no-build-isolation --no-cache-dir "
-        "bottleneck"
-    )
-
 from bottleneck.benchmark.bench import bench
 from bottleneck.benchmark.bench_detailed import bench_detailed
 from bottleneck.tests.util import get_functions
 
+from . import slow
 from ._pytesttester import PytestTester
+from .move import (move_argmax, move_argmin, move_max, move_mean, move_median,
+                   move_min, move_rank, move_std, move_sum, move_var)
+from .nonreduce import replace
+from .nonreduce_axis import (argpartition, nanrankdata, partition, push,
+                             rankdata)
+from .reduce import (allnan, anynan, median, nanargmax, nanargmin, nanmax,
+                     nanmean, nanmedian, nanmin, nanstd, nansum, nanvar, ss)
 
 test = PytestTester(__name__)
 del PytestTester
@@ -53,3 +19,6 @@ from ._version import get_versions  # noqa: E402
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
